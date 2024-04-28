97fd8815a3dff81ce893b02b09c7ff059d2b3ae0
JuliaKalaman <kalaman.julia@gmail.com>
2024-04-28 18:25:14 +0200

    add hw3 tasks 1-4

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+//Завдання 1: Оголошення змінних для примітивних типів
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+let name = 'Julia'; //рядок
+let age = 32; //число
+let isFemale = true; //буль
+let job = null; //null
+let location; //undefined
+
+console.log(name);
+console.log(age);
+console.log(isFemale);
+console.log(job);
+console.log(location)
