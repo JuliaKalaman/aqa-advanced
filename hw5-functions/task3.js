df436890844ba7b89b07dcf29f2ffa92fe095d51
JuliaKalaman <kalaman.julia@gmail.com>
2024-05-30 17:36:25 +0200

    test hooks

--- hw5-functions/task3.js
+++ hw5-functions/task3.js
@@ -8,36 +8,35 @@
 
 // Варіант 1
 function checkOrder1(available, ordered) {
-    if (available < ordered) {
-        return console.log("Your order is too large, we don't have enough goods.")
-    }
-    if (ordered === 0) {
-        return console.log('Your order is empty')
-    }
-    if (ordered <= available) {
-        return console.log('Your order is accepted')
-    }
-};
+	if (available < ordered) {
+		return console.log("Your order is too large, we don't have enough goods.");
+	}
+	if (ordered === 0) {
+		return console.log('Your order is empty');
+	}
+	if (ordered <= available) {
+		return console.log('Your order is accepted');
+	}
+}
 
-checkOrder1(10, 2);
-checkOrder1(10, 10);
-checkOrder1(10, 11);
-checkOrder1(10, 0);
+checkOrder1(10, 2)
+checkOrder1(10, 10)
+checkOrder1(10, 11)
+checkOrder1(10, 0)
 
 // Варіант 2
 function checkOrder2(available, ordered) {
-    switch (true) {
-        case available < ordered:
-            console.log("Your order is too large, we don't have enough goods.")
-            break;
-        case ordered === 0:
-            console.log('Your order is empty')
-            break;
-        case ordered <= available:
-            console.log('Your order is accepted')
-            break
-
-    }
+	switch (true) {
+		case available < ordered:
+			console.log("Your order is too large, we don't have enough goods.");
+			break;
+		case ordered === 0:
+			console.log('Your order is empty');
+			break;
+		case ordered <= available:
+			console.log('Your order is accepted');
+			break;
+	}
 }
 
 checkOrder2(10, 2);
