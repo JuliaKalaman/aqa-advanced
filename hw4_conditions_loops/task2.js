c43f20c5689b4b8c144bede038eb20814fc17cda
JuliaKalaman <kalaman.julia@gmail.com>
2024-05-30 17:24:12 +0200

    test hooks 2

--- hw4_conditions_loops/task2.js
+++ hw4_conditions_loops/task2.js
@@ -10,20 +10,21 @@
 
 let averageGrade = 85;
 switch (true) {
-    case averageGrade >= 0 && averageGrade < 60:
-        console.log("Незадовільно")
-        break;
-    case averageGrade >= 60 && averageGrade <= 70:
-        console.log("Задовільно")
-        break;
-    case averageGrade >= 71 && averageGrade <= 80:
-        console.log("Добре")
-        break;
-    case averageGrade >= 81 && averageGrade <= 90:
-        console.log("Дуже добре")
-        break;
-    case averageGrade >= 91 && averageGrade <= 100:
-        console.log("Відмінно")
-        break;
-    default: console.log("Оцінка знаходиться поза межами заданого діапазону від 0 до 100.")
+	case averageGrade >= 0 && averageGrade < 60:
+		console.log('Незадовільно');
+		break;
+	case averageGrade >= 60 && averageGrade <= 70:
+		console.log('Задовільно');
+		break;
+	case averageGrade >= 71 && averageGrade <= 80:
+		console.log('Добре');
+		break;
+	case averageGrade >= 81 && averageGrade <= 90:
+		console.log('Дуже добре');
+		break;
+	case averageGrade >= 91 && averageGrade <= 100:
+		console.log('Відмінно');
+		break;
+	default:
+		console.log('Оцінка знаходиться поза межами заданого діапазону від 0 до 100.');
 }
